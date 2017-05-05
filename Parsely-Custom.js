4a854c840f54fdfd22692cd687696e3991473766
Garrett Griess <github@thebluepig.com>
2017-05-04 19:29:31 -0500

    Update Parsely-Custom.js

--- Parsely-Custom.js
+++ Parsely-Custom.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
     setInterval(function () {
       $('#main, .item-listing').css('background-color', '#090f14'); //posts page
       $('.left-col, .right-col, .panel-list').css('background-color', '#090f14'); //overview page
+      $('.simple-title').css('font-size', '0.8em'); //Smaller post titles
       $('.post-row, .meta-row').css('background-color', '#1e2124'); //gray for anything not removed after this line
       $('a[href*="/obituaries"], a[href*="/realestate"], a[href*="/classifieds"], a[href*="/marketplace"]').closest('.post-row, .meta-row').css('background-color', '#090f14'); //removed sections
       $('a[href*="/authors/Staff"]').closest('.meta-row').css('background-color', '#090f14'); //remove Staff from authors but not posts
