b22da915397e09a481beda2299b7077b5cc0c3f4
Garrett Griess <github@thebluepig.com>
2017-05-25 10:08:51 -0500

    Update Parsely-Custom.js

--- Parsely-Custom.js
+++ Parsely-Custom.js
@@ -9,10 +9,11 @@ $(document).ready(function () {
       if (window.location.href.indexOf('fs=1') > - 1) {
         $('#main, .item-listing').css('background-color', '#090f14'); //posts page
         $('.left-col, .right-col, .panel-list').css('background-color', '#090f14'); //overview page
-        $('.simple-title').css('font-size', '0.8em'); //Smaller post titles
         $('.post-row, .meta-row').css('background-color', '#1e2124'); //gray for anything not removed after this line
         $('a[href*="/obituaries"], a[href*="/realestate"], a[href*="/classifieds"], a[href*="/marketplace"], a[href*="/epaper"], a[href*="/eedition"]').closest('.post-row, .meta-row').css('background-color', '#090f14'); //removed sections
         $('a[href*="/authors/Staff"]').closest('.meta-row').css('background-color', '#090f14'); //remove Staff from authors but not posts
+        $('.simple-title').css('font-size', '0.8em'); //Smaller post titles
+        $('.metrics-block .label').css('margin-bottom', '5px'); //Remove large gap between items on left side
         $('.units').css({ //make units stand out
           'color': '#449ad8',
           'font-size': '1.5em'
